ea81d7e78285fa06c86bdefcc11ad2b8bbe71f20
Leonel <leonelaf12@gmail.com>
2019-06-04 16:21:59 -0400

    all CRUD operations working and extracted files

--- zoos/zoos-model.js
+++ zoos/zoos-model.js
@@ -12,8 +12,35 @@ const db = knex(knexConfig);
 
 module.exports = {
     find,
-}
+    findById,
+    add,
+    update,
+    remove,
+};
 
 function find() {
     return db('zoos')
 };
+
+function findById(id) {
+    return db('zoos')
+    .where({ id })
+    .first();
+}
+
+function add(animal) {
+    return db('zoos')
+    .insert(animal, 'id')
+}
+
+function update(id, changes) {
+    return db('zoos')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+    return db('zoos')
+    .where({ id } )
+    .del();
+}
--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -4,12 +4,70 @@ const Zoos = require('./zoos-model.js');
 
 router.get('/', (req, res) => {
     Zoos.find()
-    .then( zoos => {
-        res.status(200).json(zoos);
+    .then( animals => {
+        res.status(200).json(animals);
     })
     .catch(error => {
         res.status(500).json(error);
     })
 });
+
+router.get('/:id', (req, res) => {
+    Zoos.findById(req.params.id)
+    .then( animal => {
+        res.status(200).json(animal);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+});
+
+router.post('/', (req, res) => {
+    Zoos.add(req.body)
+    .then( animal => {
+        res.status(201).json(animal);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+});
+
+router.put('/:id', (req, res) => {
+    Zoos.update(req.params.id, req.body)
+    .then( count => {
+        if(count) {
+            res.status(200).json({
+                message: `${count} records updated`
+            })
+        } else {
+            res.status(404).json({
+                message: "That animal does not exist"
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Zoos.remove(req.params.id)
+    .then( count => {
+        if(count) {
+            res.status(200).json({
+                message: `${count} record was deleted`
+            })
+        } else {
+            res.status(404).json({
+                message: "That animal was not found"
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+})
+
+
 
 module.exports = router;
